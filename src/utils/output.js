bcb23b603a667fa0c7c1a76af6cdadb61e647a7e
ber-bernado <bernado.soeryo@tokopedia.com>
2022-05-18 10:34:48 +0700

    update

--- src/utils/output.js
+++ src/utils/output.js
@@ -1,5 +1,5 @@
 const core = require('@actions/core')
-const { getLinks, getAssertionResults, getManifest } = require('./lhci-helpers')
+const { getLinks, getManifest } = require('./lhci-helpers')
 
 /**
  * Set output: {
@@ -14,11 +14,9 @@ const { getLinks, getAssertionResults, getManifest } = require('./lhci-helpers')
 
 exports.setOutput = async function setOutput(resultsPath) {
   const links = await getLinks(resultsPath)
-  const assertionResults = await getAssertionResults(resultsPath)
   const manifestResults = await getManifest(resultsPath)
 
   core.setOutput('resultsPath', resultsPath)
   core.setOutput('links', links ? JSON.stringify(links) : '')
-  core.setOutput('assertionResults', assertionResults ? JSON.stringify(assertionResults) : '')
   core.setOutput('manifest', manifestResults ? JSON.stringify(manifestResults) : '')
 }
